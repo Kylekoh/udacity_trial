c16bfd5b6a4a4c886a660b6651dc90ee040d507f
kyle_koh <ykoh728@gmail.com>
2018-09-19 17:03:41 +0900

    add controlled component

--- 5. Building With React/contacts/src/App.js
+++ 5. Building With React/contacts/src/App.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router-dom'
-import ListContacts from './ListContacts'
-import CreateContact from './CreateContact'
 import * as ContactsAPI from './utils/ContactsAPI'
+import ListContacts from './ListContacts'
 
 class App extends Component {
   state = {
@@ -13,6 +11,7 @@ class App extends Component {
       this.setState({ contacts })
     })
   }
+
   removeContact = (contact) => {
     this.setState((state) => ({
       contacts: state.contacts.filter((c) => c.id !== contact.id)
@@ -21,32 +20,14 @@ class App extends Component {
     ContactsAPI.remove(contact)
   }
 
-  createContact(contact) {
-    ContactsAPI.create(contact).then(contact => {
-      this.setState(state => ({
-        contacts: state.contacts.concat([ contact ])
-      }))
-    })
-  }
-
   render() {
     return (
       <div>
-        <Route exact path='/' render={() => (
-          <ListContacts
-            onDeleteContact={this.removeContact}
-            contacts={this.state.contacts}
-          />
-        )}/>
-        <Route path='/create' render={({ history }) => (
-          <CreateContact
-            onCreateContact={(contact) => {
-              this.createContact(contact)
-              history.push('/')
-            }}
-          />
-        )}/>
-      </div>
+        <ListContacts 
+          onDeleteContact={this.removeContact} 
+          contacts={this.state.contacts} 
+        />
+      </div>  
     )
   }
 }
