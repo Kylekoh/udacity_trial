504f0d3e09e36953897cecba661c17638a752838
kyle_koh <ykoh728@gmail.com>
2018-09-25 00:13:07 +0900

    add shelves to control shelf array

--- 5. Building With React/reactnd-project-myreads-starter-master/src/BookShelf.js
+++ 5. Building With React/reactnd-project-myreads-starter-master/src/BookShelf.js
@@ -1,27 +1,45 @@
 import React, { Component } from 'react';
 import BookList from './BookList';
 
+const shelves = [
+  	{
+  		key: 'currentlyReading',
+  		name: 'Currently Reading'
+  	},
+  	{
+  		key: 'wantToRead',
+  		name: 'Want To Read'
+  	},
+  	{
+  		key: 'read',
+  		name: 'Read'
+  	}
+  ]
 
 class BookShelf extends Component {
+
+
   render() {
   	const { books } = this.props;
 
     return (
       <div className="list-books-content">
-        <div>      
-          <div className="bookshelf">
-            <h2 className="bookshelf-title">Currently Reading</h2>
-            <div className="bookshelf-books">      
-              <ol className="book-grid"> 
-                {books.map((book) => (
-                  <li key={book.id} className="contact-list-item">
-                    <BookList book={book}/>
-                  </li>
-                ))}
-              </ol>
-            </div>
-          </div>
-        </div>       
+        <div>
+        	{shelves.map((shelf) => (    
+	          <div className="bookshelf">
+	            <h2 className="bookshelf-title">{shelf.name}</h2>
+	            <div className="bookshelf-books">      
+	              <ol className="book-grid"> 
+	                {books.map((book) => (
+	                  <li key={book.id} className="contact-list-item">
+	                    <BookList book={book}/>
+	                  </li>
+	                ))}
+	              </ol>
+	            </div>
+	          </div>
+	        ))}    
+	    </div>       
       </div>  
     );
   }
